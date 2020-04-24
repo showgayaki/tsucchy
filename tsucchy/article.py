09cbcfc3015f514db9ea0068874cfca0b75c3b6a
mo22comi <knk.lee.kj@gmail.com>
2020-04-24 17:06:51 +0900

    article.pyの要素取得まわり修正

--- tsucchy/article.py
+++ tsucchy/article.py
@@ -22,14 +22,14 @@ class LatestArticles:
         try:
             r = requests.get(self.target_url, timeout=(3, 6))
             if r.status_code == 200:
-                soup = BeautifulSoup(r.text, 'html.parser')
-                latest_articles = soup.find(id=self.target_id).find_all('a')
+                soup = BeautifulSoup(r.content, 'html.parser')
+                latest_articles = soup.select('#{} a'.format(self.target_id))
 
-                for i in range(len(latest_articles)):
+                for i, elem in enumerate(latest_articles):
                     key = 'article_{}'.format(i + 1)
                     articles_dict[key] = {}
-                    articles_dict[key]['title'] = latest_articles[i].contents[0]
-                    articles_dict[key]['link'] = latest_articles[i].attrs['href']
+                    articles_dict[key]['title'] = elem.contents[0]
+                    articles_dict[key]['link'] = elem.attrs['href']
             else:
                 articles_dict['error'] = 'Status Code:{} From:{}'.format(r.status_code, self.target_url)
         except Exception as e:
